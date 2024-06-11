8298f45cafad0c4f02ff2c054e102d85f7fe12dc
SaubinT <titouan.saubin@etu.univ-st-etienne.fr>
2024-06-11 18:45:59 +0200

    docker file pas fonctionnel mais là

--- Portfolio/Portfolio.ps1
+++ Portfolio/Portfolio.ps1
@@ -1,3 +1,2 @@
 docker build -t serv_portfo C:\\Users\\asusr\\Documents\\SAE203\\Portfolio
-docker run --name Portfolio_SAUBIN -p 82:80 -tid serv_portfo7
-start http://localhost:82
+docker run --name Portfolio_SAUBIN -p 82:80 -tid serv_portfo
