a03a894af04d38f41bb6dd62e60dfa814e2fac85
SaubinT <titouan.saubin@etu.univ-st-etienne.fr>
2024-06-14 22:20:01 +0200

    Fin Des TP selon moi

--- Portfolio/Portfolio.ps1
+++ Portfolio/Portfolio.ps1
@@ -1,2 +1,3 @@
-docker build -t serv_portfo C:\\Users\\asusr\\Documents\\SAE203\\Portfolio
+docker build -t serv_portfo .
 docker run --name Portfolio_SAUBIN -p 82:80 -tid serv_portfo
+docker exec -ti Portfolio_SAUBIN composer.phar -n install
